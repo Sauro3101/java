ca67a931cee87ceb7db223b6bcfe46a6b8e8c17f
Sauro3101 <91719731+Sauro3101@users.noreply.github.com>
2021-10-01 08:48:28 +0200

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,22 @@
+public class Perosna{
+	private String nome;
+	private int eta;
+	//costruttore
+	public Persona(String nome, int eta){
+		setNome(nome);
+		setEta(eta);
+	}
+	//setter/getter
+	public int getEta(){
+		return eta;
+	}
+	private void setEta(int e){
+		eta=e;
+	}
+	public String getNome(){
+		return nome;
+	}
+	private void setNome(String n){
+		nome=n;
+	}
+}
