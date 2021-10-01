aed157f97a708feb8759678a786c49dec13a2561
Giulio <91719917+bmbSchoolGit@users.noreply.github.com>
2021-10-01 09:08:43 +0200

    Update prog.java

--- prog.java
+++ prog.java
@@ -1,7 +1,8 @@
-pubblic static void main(String[]args){
-	Persona p;
-	p=new Televisore("Mattia",17);
-	System.out.println(p.getNome);
-	System.out.println(p.getEta);
+public class prog{
+	public static void main(String[]args){
+		Persona p;
+		p=new Televisore("Mattia",17);
+		System.out.println(p.getNome);
+		System.out.println(p.getEta);
+	}
 }
-	
