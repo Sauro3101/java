4d72290cecb106482fb101cdcd7e8150f1cfac77
Giulio <91719917+bmbSchoolGit@users.noreply.github.com>
2021-10-01 09:09:55 +0200

    updating main file

--- prog.java
+++ prog.java
@@ -1,6 +1,6 @@
 public class prog{
 	public static void main(String[]args){
-		Persona p;
+		Persona p1, p2;
 		p=new Televisore("Mattia",17);
 		System.out.println(p.getNome);
 		System.out.println(p.getEta);
